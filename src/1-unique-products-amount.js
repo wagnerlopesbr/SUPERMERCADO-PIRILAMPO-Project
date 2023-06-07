6efa636238ef06da0ad218e3c3d046fa32582ee0
Wagner Lopes <wagnerlopesbr@hotmail.com>
2023-06-07 13:49:04 -0300

    WL 1/10 v4

--- src/1-unique-products-amount.js
+++ src/1-unique-products-amount.js
@@ -2,6 +2,15 @@ const stockProducts = require('./data.json');
 
 const getUniqueProductsAmount = () => {
   // Desenvolva seu código dentro dessa função...
+  let products = [];
+  let uniqueProducts = 0;
+  for (let i = 0; i < stockProducts.length; i += 1) {
+    products.push(stockProducts[i]);
+    if (products !== stockProducts) {
+      uniqueProducts += 1;
+    }
+  }
+  return uniqueProducts;
 };
 
 module.exports = { getUniqueProductsAmount };
