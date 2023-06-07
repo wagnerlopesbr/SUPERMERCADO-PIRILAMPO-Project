01d4b827ffcf21905369256fc93a12b2106aba98
Wagner Lopes <wagnerlopesbr@hotmail.com>
2023-06-07 20:39:32 -0300

    WL - 2/10

--- src/2-unique-products-name.js
+++ src/2-unique-products-name.js
@@ -2,6 +2,13 @@ const stockProducts = require('./data.json');
 
 const getUniqueProductsName = () => {
   // Desenvolva seu código dentro dessa função...
+  let product;
+  let uniqueProducts = [];
+  for (let i = 0; i < stockProducts.length; i += 1) {
+    product = stockProducts[i].productName;
+    uniqueProducts.push(product);
+  }
+  return uniqueProducts;
 };
 
 module.exports = { getUniqueProductsName };
