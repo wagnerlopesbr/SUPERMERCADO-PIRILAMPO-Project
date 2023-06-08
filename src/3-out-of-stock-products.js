b6d6b425e309a2ea5e413b7eb4d92b81e18e6061
Wagner Lopes <wagnerlopesbr@hotmail.com>
2023-06-08 08:13:44 -0300

    WL 3/10

--- src/3-out-of-stock-products.js
+++ src/3-out-of-stock-products.js
@@ -2,6 +2,15 @@ const stockProducts = require('./data.json');
 
 const getOutOfStockProducts = () => {
   // Desenvolva seu código dentro dessa função...
+  let product;
+  let offProducts = [];
+  for (let i = 0; i < stockProducts.length; i += 1) {
+    product = stockProducts[i];
+    if (product.quantityInStock === 0) {
+      offProducts.push(stockProducts[i].productName);
+    }
+  }
+  return offProducts;
 };
 
 module.exports = { getOutOfStockProducts };
