87ab1c2457b456a5e90efebd4413532dc630f37b
Wagner Lopes <wagnerlopesbr@hotmail.com>
2023-06-08 09:21:28 -0300

    WL 5/10

--- src/4-low-stock-products.js
+++ src/4-low-stock-products.js
@@ -2,6 +2,15 @@ const stockProducts = require('./data.json');
 
 const getLowStockProducts = () => {
   // Desenvolva seu código dentro dessa função...
+  let product;
+  let lowStockProducts = [];
+  for (let i = 0; i < stockProducts.length; i += 1) {
+    product = stockProducts[i];
+    if (product.quantityInStock > 0 && product.quantityInStock <= 10) {
+      lowStockProducts.push(`${product.productName}: ${product.quantityInStock} unidades.`);
+    }
+  }
+  return lowStockProducts;
 };
 
 module.exports = { getLowStockProducts };
--- src/5-products-amount.js
+++ src/5-products-amount.js
@@ -2,6 +2,13 @@ const stockProducts = require('./data.json');
 
 const getProductsAmount = () => {
   // Desenvolva seu código dentro dessa função...
+  let product;
+  let totalAmount = 0;
+  for (let i = 0; i < stockProducts.length; i += 1) {
+    product = stockProducts[i].quantityInStock;
+    totalAmount += product;
+  }
+  return totalAmount;
 };
 
 module.exports = { getProductsAmount };
