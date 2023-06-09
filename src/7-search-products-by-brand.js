de2c2f4c7f3132555490b1b0b86221c1882e92e9
Wagner Lopes <wagnerlopesbr@hotmail.com>
2023-06-09 13:54:41 -0300

    WL 7/10

--- src/7-search-products-by-brand.js
+++ src/7-search-products-by-brand.js
@@ -1,7 +1,21 @@
 const stockProducts = require('./data.json');
 
-const searchProductsByBrand = () => {
+const searchProductsByBrand = (itemBrand) => {
   // Desenvolva seu código dentro dessa função...
+  let product;
+  let productBrand = {};
+  let productArray = [];
+  for (let i = 0; i < stockProducts.length; i += 1) {
+    product = stockProducts[i];
+    if (product.brand !== 'undefined' && product.brand === itemBrand) {
+      productBrand = {
+        'description': product.description,
+        'formattedPrice': `R$ ${product.price.toFixed(2)}`
+      };
+      productArray.push(productBrand);
+    }
+  }
+  return productArray;
 };
 
 module.exports = { searchProductsByBrand };
