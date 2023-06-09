c24754eb6de27eba9ceac834e73b633ead7570fb
Wagner Lopes <wagnerlopesbr@hotmail.com>
2023-06-09 14:14:36 -0300

    WL 8/10

--- src/8-products-on-sale.js
+++ src/8-products-on-sale.js
@@ -2,6 +2,20 @@ const stockProducts = require('./data.json');
 
 const getProductsOnSale = () => {
   // Desenvolva seu código dentro dessa função...
+  let product;
+  let productsOnSale = [];
+  for (let i = 0; i < stockProducts.length; i += 1) {
+    product = stockProducts[i];
+    if (product.onSale === true) {
+      let productsInfo = {
+        'description': product.description,
+        'formattedPrice': `R$ ${product.price.toFixed(2)}`,
+        'onSale': product.onSale
+      }
+      productsOnSale.push(productsInfo);
+    }
+  }
+  return productsOnSale;
 };
 
 module.exports = { getProductsOnSale };
