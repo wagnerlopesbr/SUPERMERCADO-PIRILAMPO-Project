4db11202044744bf0393fc3f9b033e62ffda8447
Wagner Lopes <wagnerlopesbr@hotmail.com>
2023-06-09 14:54:18 -0300

    WL 9/10

--- src/9-products-with-allergy-or-intolerance-info.js
+++ src/9-products-with-allergy-or-intolerance-info.js
@@ -2,6 +2,20 @@ const stockProducts = require('./data.json');
 
 const getProductsWithAllergyOrIntoleranceInfo = () => {
   // Desenvolva seu código dentro dessa função...
+  let product;
+  let productsWithAorIInfo = [];
+  for (let i = 0; i < stockProducts.length; i += 1) {
+    product = stockProducts[i];
+    if (product.allergyOrIntolerance !== undefined) {
+      let productsWithAlergyOrIntoleranceInfos = {
+        'description': product.description,
+        'formattedPrice': `R$ ${product.price.toFixed(2)}`,
+        'allergyOrIntoleranceMessage': `Pode conter: ${product.allergyOrIntolerance.join(" ").toString()}`
+      }
+      productsWithAorIInfo.push(productsWithAlergyOrIntoleranceInfos);
+    }
+  }
+  return productsWithAorIInfo;
 };
 
 module.exports = { getProductsWithAllergyOrIntoleranceInfo };
