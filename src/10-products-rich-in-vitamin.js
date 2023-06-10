43b3488b73e14629e1a7b4e0c141653bc8fc3ed8
Wagner Lopes <wagnerlopesbr@hotmail.com>
2023-06-09 21:25:20 -0300

    WL 10/10

--- src/10-products-rich-in-vitamin.js
+++ src/10-products-rich-in-vitamin.js
@@ -2,6 +2,21 @@ const stockProducts = require('./data.json');
 
 const getProductsRichInVitamin = () => {
   // Desenvolva seu código dentro dessa função...
+  let product;
+  let vitaminsInfo = [];
+  for (let i = 0; i < stockProducts.length; i += 1) {
+    product = stockProducts[i];
+    if (product.nutritionalInfo.vitamins !== undefined) {
+      let vitaminasTunadas = new Array(product.nutritionalInfo.vitamins);
+      let productsInfo = {
+        'description': product.description,
+        'formattedPrice': `R$ ${product.price.toFixed(2)}`,
+        'vitaminsInformation': vitaminasTunadas
+      }
+      vitaminsInfo.push(productsInfo);
+    }
+  }
+  return vitaminsInfo;
 };
 
 module.exports = { getProductsRichInVitamin };
